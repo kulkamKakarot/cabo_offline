e66c39c1fc6ef59330aa62229a82f137c8667815
Aditya Chaudhary <adityamacromaniax@gmail.com>
2025-03-09 11:17:03 +0530

    Update Player.dart

--- lib/Player.dart
+++ lib/Player.dart
@@ -5,10 +5,14 @@ class Player {
   final String name;
   List<PlayingCard> hand = [];
   bool calledCabo = false;
+  int totalScore = 0;
+  int roundScore = 0;
 
   Player({required this.name});
 
   int calculateScore() {
-    return hand.fold(0, (sum, card) => sum + card.points);
+    // Calculate score based on cards in hand
+    roundScore = hand.fold(0, (sum, card) => sum + card.points);
+    return roundScore;
   }
 }
